ab7f72c9d733af7938ed2e756a058905ef7cfdd2
Semm6 <sem.dewilde6@gmail.com>
2021-12-09 22:44:15 +0100

    Made new repo for research and portfolio

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import "../Style/account.css";
 
 const Account = () => {
+  const { user, isAuthenticated } = useAuth0();
 
-    const { user, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && ( //see if user is authenticated (if user is logged in show data)
+      <div>
+        <h1>Account information</h1>
 
-    return (
-      isAuthenticated && ( //see if user is authenticated (if user is logged in show data)
-        <div>
-            <h1>Account information</h1>
-
-            <div className="picture">
-              <img src={user.picture} alt="userpic"/>
-            </div>
-
-            <h3>{user.name}</h3>
-            <a>{user.email}</a>
-            <a>{user.locale}</a>
-
+        <div className="picture">
+          <img src={user.picture} alt="userpic" />
         </div>
-      )
-    )
-}
 
-export default Account
+        <h3>{user.name}</h3>
+        <a>{user.email}</a>
+        <a>{user.locale}</a>
+      </div>
+    )
+  );
+};
+
+export default Account;
